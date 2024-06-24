804503a9afc40f4cbf485c0d88121b45196e6a59
Davronnnn <kifleader@gmail.com>
2024-06-24 20:55:36 +0500

    lesson: 2-dars

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-var number = 123.01
-var result = Math.min(12,123,12,3,-4)
-// Math.floor(Math.random() * (max - min + 1) + min)
- 
+var age = prompt("Yoshingizni kiriting");
+// || yoki
+// Agar chap taraf truthy qiymat bo'lsa chap tarafni qaytaradi aks holda o'ng tarafni qaytaradi
+// && va
+// Agar chap taraf truthy qiymat bo'lsa o'ng tarafni qaytaradi aks holda chap tarafni qaytaradi
+
+console.log(false || true);
+// and &&
+
+// true true -> true
+// true false -> false
+// false true -> false
+// false false -> false
+
+// yoki ||
+
+// true true -> true
+// true false -> true
+// false true -> true
+// false false -> false
+
+if (age >= 20) {
+    var sum = prompt("Pulingizni kiriting");
+    
+    if (sum > 2000) {
+        console.log("Siz konsertga kira olasiz");
+    }else{
+        console.log("Siz bilet sotib ola olmaysiz");
+    }
+} else {
 
 
-console.log(12/5);
+    console.log("Siz konsertga kira olmaysiz");
+}
