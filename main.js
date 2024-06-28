e06c595e10a14d7017c23047abf94c27248f031d
Davronnnn <kifleader@gmail.com>
2024-06-28 20:51:01 +0500

    fix: lesson

--- main.js
+++ main.js
@@ -1,34 +1,76 @@
-var age = prompt("Yoshingizni kiriting");
-// || yoki
-// Agar chap taraf truthy qiymat bo'lsa chap tarafni qaytaradi aks holda o'ng tarafni qaytaradi
-// && va
-// Agar chap taraf truthy qiymat bo'lsa o'ng tarafni qaytaradi aks holda chap tarafni qaytaradi
+const elInput = document.querySelector("input");
+const elBtn = document.querySelector("#btn");
+const elResult = document.querySelector("#result");
+const elWrapper = document.querySelector(".wrapper");
+let salom = "dunyo";
 
-console.log(false || true);
-// and &&
+elBtn.addEventListener("click", function () {
+    const score = Number(elInput.value);
+    let content = elResult.textContent;
+    let textColor = elResult.style.color;
+   
+    if (score > 0 && score < 80) {
+        content = "Siz yiqildingiz";
+        textColor = "red";
+    } else if (score >= 80 && score < 100) {
+        content = "Siz super kontraktga kirdingiz";
+        textColor = "blue";
 
-// true true -> true
-// true false -> false
-// false true -> false
-// false false -> false
+        const resultTeg = document.createElement("p");
+        const resultInput = document.createElement("input");
+        const resultBtn = document.createElement("button");
+        resultTeg.textContent = "Super kontrakt narxi 3000$ iltimos summani kiriting";
 
-// yoki ||
+        resultTeg.className = "result"
+        resultBtn.textContent ="Submit"
 
-// true true -> true
-// true false -> true
-// false true -> true
-// false false -> false
+        resultBtn.addEventListener("click", function(){
+            if(resultInput.value >=3000){
+                content = "siz o'qishga kirdingiz"
+            }else(
+                content = "mablag'ingiz yetmadi"
+            )
+        })
+       
+        elWrapper.appendChild(resultTeg);
+        elWrapper.appendChild(resultInput);
+        elWrapper.appendChild(resultBtn);
 
-if (age >= 20) {
-    var sum = prompt("Pulingizni kiriting");
-    
-    if (sum > 2000) {
-        console.log("Siz konsertga kira olasiz");
-    }else{
-        console.log("Siz bilet sotib ola olmaysiz");
+
+    } else if (score >= 100 && score < 130) {
+        content = "Siz  kontraktga kirdingiz";
+        textColor = "blue";
+
+
+        const resultTeg = document.createElement("p");
+        const resultInput = document.createElement("input");
+        const resultBtn = document.createElement("button");
+        resultTeg.textContent = "kontrakt narxi 2000$ iltimos summani kiriting";
+
+        resultBtn.textContent ="Submit"
+
+        resultBtn.addEventListener("click", function(){
+            if(resultInput.value >=2000){
+                content = "siz o'qishga kirdingiz"
+            }else(
+                content = "mablag'ingiz yetmadi"
+            )
+        })
+
+        elWrapper.appendChild(resultTeg);
+        elWrapper.appendChild(resultInput);
+        elWrapper.appendChild(resultBtn);
+
+    } else if (score >= 130 && score <= 180) {
+        content = "Siz  grant asosida kiritdingiz kirdingiz";
+        textColor = "green";
+    } else {
+        content = "Noto'g'ri qiymat kiritildi";
+        textColor = "blue";
     }
-} else {
 
 
-    console.log("Siz konsertga kira olmaysiz");
-}
+    elResult.textContent = content;
+    elResult.style.color = textColor;
+
+});
