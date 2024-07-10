0f79403924ce708323afdceeeffe9deae059fc36
Davronnnn <kifleader@gmail.com>
2024-07-10 19:51:08 +0500

    fix: changes

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-//function declaration
-function calculateHyponatize(a,b) { // parametr
-    return  Math.sqrt(a*a + b*b);
-} // hoisting 
+const books = [
+    {
+        title: "Sariq devni minib",
+        author: "Xudoyberdi To'xtaboyev",
+        year: 2100,
+        genre: "fiction",
+    },
+    {
+        title: "Kichkina shahzoda",
+        author: "Antuan de sent",
+        year: 1800,
+        genre: "comedy",
+    },
+    {
+        title: "Qo'rqma",
+        author: "Javlon Jovliyev",
+        year: 2018,
+        genre: "horror",
+    },
+    {
+        title: "Mehrobdan chayon",
+        author: "Abdulla Qodiriy",
+        year: 2015,
+        genre: "fiction",
+    },
+];
 
-//function expression
-const calculate = function asd(a,b) {
-    return  "test";
-} 
+function getNewBooks(elements) {
+    const newElements = [];
 
-// function arrow function
-const calculate2 = (a,b) => {
-    return "test2"
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].year > 2010) {
+            newElements.push(elements[i]);
+        }
+    }
+    return newElements;
 }
-console.log(calculateHyponatize(3,4))
-console.log(calculate(3,4))
-console.log(calculate2(3,4)) // argument
